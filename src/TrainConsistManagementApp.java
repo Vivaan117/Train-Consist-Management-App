eeb44cce342fa2755f49990c7a428faa1b3c3482
Vivaan117 <shethvivaan17@gmail.com>
2026-04-07 09:03:18 +0530

    UC10: Calculate Total Seating Capacity using reduce

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -11,29 +11,21 @@ class Bogie {
     }
 }
 
-public class UC9_GroupBogies {
+public class UC10_TotalSeats {
     public static void main(String[] args) {
 
         // Step 1: Create list
         List<Bogie> bogies = new ArrayList<>();
         bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("AC Chair", 56));
-        bogies.add(new Bogie("Sleeper", 72));
         bogies.add(new Bogie("First Class", 24));
 
-        // Step 2: Group using Stream
-        Map<String, List<Bogie>> grouped =
-                bogies.stream()
-                        .collect(Collectors.groupingBy(b -> b.name));
+        // Step 2: Aggregate using map + reduce
+        int totalSeats = bogies.stream()
+                .map(b -> b.capacity)
+                .reduce(0, Integer::sum);
 
-        // Step 3: Display grouped data
-        System.out.println("Grouped Bogies:");
-
-        for (String key : grouped.keySet()) {
-            System.out.println("\n" + key + ":");
-            for (Bogie b : grouped.get(key)) {
-                System.out.println("Capacity -> " + b.capacity);
-            }
-        }
+        // Step 3: Display result
+        System.out.println("Total Seating Capacity: " + totalSeats);
     }
 }
